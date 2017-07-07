e94ec2ae06a6274813b0cd4f5d55657eb17726cc
shelbina <deel.shelby@gmail.comgit clone git@github.com:shelbina/mayeleven.gitgit@github.com:shelbina/mayeleven.git$ git config --global user.email deel.shelby@gmail.com>
2017-07-07 17:28:40 -0400

    update firebase on js

--- assets/script.js
+++ assets/script.js
@@ -12,26 +12,61 @@
 
   $("#submit").on("click", function(event) {
   event.preventDefault();
-
+  //take user input
   var rideColor = $("#ride-color").val().trim();
   var rideDestination = $("#ride-destination").val().trim();
   var rideFirstTime = moment($("#ride-firstTime").val().trim(), "DD/MM/YY").format("X");
   var rideFrequency = $("#ride-frequency").val().trim();
   var rideArrival = $("#ride-arrival").val().trim();
   var rideAway = $("#ride-away").val().trim();
-
+  //local object to store data
   var newRide = {
-  	Color: rideColor,
+  Color: rideColor,
 	destination: rideDestination,
+  firstTime:rideFirstTime,
 	frequency: rideFrequency,
 	arrival: rideArrival,
 	away: rideAway,
   };
-
+  //upload to database
   database.ref().push(newRide);
-
+  //log to console
   console.log(newRide.color);
   console.log(newRide.destination);
+  console.log(newRide.firstTime);
   console.log(newRide.frequency);
   console.log(newRide.arrival);
   console.log(newRide.away);
+  //alert
+  alert("Your ride has been added!")
+  //clear text input
+  $("#ride-color").val("");
+  $("#ride-destination").val("");
+  $("#ride-firstTime").val();
+  $("#ride-frequency").val();
+  $("#ride-arrival").val();
+  $("#ride-away").val();
+  
+});
+
+//create event to add rides to firebase and row in html
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+  console.log(childSnapshot.val());
+  //store into the variable
+  var rideColor = childSnapshot.val().color;
+  var rideDestination = childSnapshot.val().destination;
+  var rideFirstTime = childSnapshot.val().firstTime;
+  var rideFrequency = childSnapshot.val().frequency;
+  var rideArrival = childSnapshot.val().arrival;
+  var rideAway = childSnapshot.val().away;
+  //ride info
+  console.log(newRide.color);
+  console.log(newRide.destination);
+  console.log(newRide.firstTime);
+  console.log(newRide.frequency);
+  console.log(newRide.arrival);
+  console.log(newRide.away);
+  //calculate time in moment.js
+
+
+});
